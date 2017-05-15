6283bbabcd7dceb86043b994f5a492a4f9afad78
swim <swim@tent.(none)>
2017-05-15 10:02:23 +0200

    ADDED: a code example of using valgrind tool.

---
+++ estudio/cpp_language/code_examples/valgrind/quick_star_example/main.cpp
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+// bad code
+void f(void)
+{
+  int * x= (int*)malloc(10 * sizeof(int)); // C++ code // int* x = malloc(10 * sizeof(int));    // C code
+  x[10] = 0;        // problem 1: heap block overrun
+                    // problem 2: memory leak -- x not freed
+  int undef_value;
+  x[0] = undef_value;
+}                    
+
+
+
+
+// good code
+/*
+void f(void)
+{
+  int * x= (int*)malloc(10 * sizeof(int)); // C++ code // int* x = malloc(10 * sizeof(int));    // C code
+  x[9] = 0;        // problem 1: heap block overrun
+  free(x);
+}                    // problem 2: memory leak -- x not freed
+*/
+
+
+int main(void)
+{
+  f();
+  return 0;
+}
+
+//==============================================================================
+
+/* EOFILE */
