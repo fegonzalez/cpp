db6bacab55e98254394fc3f325f89ca3ca917cc2
swim <swim@tent.(none)>
2017-06-12 16:31:13 +0200

    UPDATED: callgrind examples.

--- estudio/cpp_language/code_examples/valgrind/callgrind/main.cpp
+++ estudio/cpp_language/code_examples/valgrind/callgrind/main.cpp
@@ -1,36 +1,59 @@
-#include <stdlib.h>
+#include <iostream>
+#include <cmath>
 
-// bad code
-void f(void)
-{
-  int * x= (int*)malloc(10 * sizeof(int)); // C++ code // int* x = malloc(10 * sizeof(int));    // C code
-  x[10] = 0;        // problem 1: heap block overrun
-                    // problem 2: memory leak -- x not freed
-  int undef_value;
-  x[0] = undef_value;
-}                    
-
-
-
-
-// good code
-/*
-void f(void)
-{
-  int * x= (int*)malloc(10 * sizeof(int)); // C++ code // int* x = malloc(10 * sizeof(int));    // C code
-  x[9] = 0;        // problem 1: heap block overrun
-  free(x);
-}                    // problem 2: memory leak -- x not freed
-*/
-
-
-int main(void)
-{
-  f();
-  
-  return 0;
-}
+// >>which valgrind
+// /usr/bin/valgrind
+//#include <valgrind/callgrind.h> //main.cpp:6:32: fatal error: valgrind/callgrind.h: No such file or directory
 
 //==============================================================================
 
-/* EOFILE */
+bool isPrime(int x)
+{
+  int limit = std::sqrt(x);
+  for (int i = 2; i <= limit; ++i)
+  {
+    if (x % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+int doble_primo(int x)
+{
+  int retval =  0;
+  for(int j=x+1; j<10000; ++j)
+    if (isPrime(x))              // another caller to isPrime
+      {
+	retval = j;
+	break;
+      }
+
+  return retval;
+}
+
+
+//==============================================================================
+
+int main()
+{
+  int primeCount = 0;
+  int doble_count =0;
+  for (int i = 0; i < 1000000; ++i)
+    {
+      if (isPrime(i))
+	{
+	  // CALLGRIND_START_INSTRUMENTATION;
+          ++primeCount;
+	  doble_count+=doble_primo(i);
+	  // CALLGRIND_STOP_INSTRUMENTATION;
+	}
+    }
+  std::cout << primeCount << " ; " << doble_count << std::endl;
+}
+ 
+//==============================================================================
+
+/*  EOFILE  */
